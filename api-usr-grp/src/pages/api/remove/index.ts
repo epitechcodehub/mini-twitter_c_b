ffea9bfc8665ceb6c3a776f8b18a1d33b719a34f
Samuel Bègue <brandon-jean-samuel.begue@epitech.eu>
2025-02-07 16:51:06 +0400

    add remove elem from database

--- api-usr-grp/src/pages/api/remove/index.ts
+++ api-usr-grp/src/pages/api/remove/index.ts
@@ -3,7 +3,7 @@ import { posts } from "../../../data/store";
 
 export default function remove(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "DELETE") {
-        const {id} = req.body;
+        const {id} = req.body; // recup l'id depuis la data base
         let can_delete = false;
         let id_int = parseInt(id, 10) - 1;
         if (posts.at(id_int) != undefined) {
